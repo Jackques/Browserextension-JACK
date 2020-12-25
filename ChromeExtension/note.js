1843aca216f7e49e7faf5349cfa6bddef1642aad
ANVA_DOMEIN\JackG <j.gommeren@anva.nl>
2020-12-25 23:31:58 +0100

    Added some usefull code in note

---
+++ ChromeExtension/note.js
@@ -0,0 +1,6 @@
+$("span[class^='pipeline-node-']").not('.pipeline-node-592').remove()
+
+/*
+Use code above to shorten the page. Jquery has been added to the page for this explicit reason.
+Maybe inplement this code on the example page (or make a shortend example page??) so it executes automatically?
+ */
